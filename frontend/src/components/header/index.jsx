e3666a26a36f097bba83bf215b72163650a195be
이재영 <42104898+JaeYeong-Li@users.noreply.github.com>
2022-02-23 21:20:28 +0900

    feat #2 헤더 마크업

---
+++ frontend/src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import './index.scss';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="header-background"></div>
+            <div className="header-current"></div>
+        </header>
+    )
+}
+
+export default Header;
