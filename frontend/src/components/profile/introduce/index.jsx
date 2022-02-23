8d9f1eeb0b88abab676cf9c53ed5399fd1c1252f
이재영 <42104898+JaeYeong-Li@users.noreply.github.com>
2022-02-23 21:23:55 +0900

    feat #3 프로필 마크업

---
+++ frontend/src/components/profile/introduce/index.jsx
@@ -0,0 +1,9 @@
+import './index.scss';
+const Introduce = () => {
+    return <div className="profile-intro">안녕하세요 웹 개발자 이재영입니다.<br/>
+    자바스크립트 기반 Frontend, Backend 개발에 관심이 있습니다.<br/>
+    책임감 있게, 소통하며, 그리고 즐겁게 협업하여<br/>
+    사용자가 몰입할 수 있는 웹을 만들고 싶습니다.</div>;
+}
+
+export default Introduce;
