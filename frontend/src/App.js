202e85b95e655a7fcc6c026c5459fc4c4d44ad53
이재영 <42104898+JaeYeong-Li@users.noreply.github.com>
2022-03-03 18:57:21 +0900

    feat #1 화면 스크롤 css 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import '@/App.scss';
 import Header from '@components/header';
-import Profile from '@components/profile'
+import Profile from '@components/profile';
+import Skill from '@components/skill';
 
 function App() {
   return (
     <div className="rootWrapper">
-    <Header/>
-    <div className="content">
-    <Profile/>
-    </div>
+      <Header/>
+      <div className="content">
+        <Profile/>
+        <Skill/>
+      </div>
     </div>
   );
 }
