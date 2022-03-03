b97aa99a0bbc130ac11f30405a4768ca44270266
이재영 <42104898+JaeYeong-Li@users.noreply.github.com>
2022-03-03 18:54:19 +0900

    ci #1 리덕스 devTool 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,9 @@ import App from '@/App';
 import {createStore} from "redux";
 import rootReducer from '@store';
 import { Provider } from 'react-redux';
-const store = createStore(rootReducer);
+
+const devTool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTool);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
