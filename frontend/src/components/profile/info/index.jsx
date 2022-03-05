945666f051d6c62a772c6b88a82dfcebcfa4ef2b
이재영 <42104898+JaeYeong-Li@users.noreply.github.com>
2022-03-05 11:47:04 +0900

    chore #3 프로필 수정

--- frontend/src/components/profile/info/index.jsx
+++ frontend/src/components/profile/info/index.jsx
@@ -1,9 +1,18 @@
 /* eslint-disable jsx-a11y/alt-text */
 import './index.scss';
 
+const myMenu = [{name:"akvk98@naver.com"}, {name:"Github", link:"https://github.com/JaeYeong-Li"}, {name:"Blog", link:"https://velog.io/@akvk98"}];
+const infoMenuClickHandler = (e) => {
+    const curName = e.target.innerHTML;
+    let idx;
+    myMenu.some((menu, i)=>{if(menu.name===curName) idx=i; return (menu.name===curName)})
+    window.open(myMenu[idx].link, '_blank');
+}
 const InfoMenu = () => {
-    const myMenu = ["akvk98@naver.com", "Github", "Blog"]
-    return <div className="profile-infoMenu">{myMenu.map((e)=><div key={e} className="moveButton"><p>{e}</p></div>)}</div>;
+    return <div className="profile-infoMenu">{myMenu.map((menu)=> {
+        return Object.keys(menu).length === 1 ? <div key={menu.name} className="moveButton">{menu.name}</div> :
+        <div key={menu.name} className="moveButton" onClick={(e) => infoMenuClickHandler(e)}>{menu.name}</div>
+    })}</div>
 }
 
 const Info = () => {
