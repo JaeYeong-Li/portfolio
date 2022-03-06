523fcd01929736560b51ee958834dd2de9d3d010
이재영 <42104898+JaeYeong-Li@users.noreply.github.com>
2022-03-06 19:00:25 +0900

    feat #9 스크롤 이벤트 작업
    - [x] 스크롤에 따라 헤더의 진행율 업데이트
    - [x] 스크롤에 따라 컴포넌트 애니메이션 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,46 @@ import Header from '@components/header';
 import Profile from '@components/profile';
 import Skill from '@components/skill';
 import Projects from '@components/projects';
+import React,{useLayoutEffect, useMemo, useRef} from 'react';
+import { useDispatch } from 'react-redux';
+import {throttle} from 'lodash';
+import { updateScroll } from '@store/modules/setting';
 
 function App() {
+  const dispatch = useDispatch();
+  const saTriggerMargin = 300;
+  const myComponents = useRef(null);
+
+  const showHandler = () => {
+    for(const element of myComponents.current.childNodes) {
+      const curScroll = element.getBoundingClientRect().top + saTriggerMargin;
+      if( ((!element.classList.contains('show')) && (window.innerHeight > curScroll)) || 
+        (element.classList.contains('show') && (window.innerHeight <= curScroll )) ) {
+          element.classList.toggle('show');
+        }
+    }
+  }
+  const throttledScrollHandler = useMemo(
+    () =>
+      throttle(() => {
+        const scrollTop = document.documentElement.scrollHeight - window.innerHeight;
+        dispatch(updateScroll(window.scrollY*100/scrollTop));
+        showHandler();
+      }, 300),
+    [dispatch]
+  );
+
+  useLayoutEffect(()=>{
+    window.addEventListener('scroll', throttledScrollHandler);
+    return ()=> {
+      window.removeEventListener(throttledScrollHandler);
+    }
+  },[throttledScrollHandler]);
+
   return (
     <div className="rootWrapper">
       <Header/>
-      <div className="content">
+      <div className="content" ref={myComponents}>
         <Profile/>
         <Skill/>
         <Projects/>
