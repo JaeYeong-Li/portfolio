f8a1430f4f02dc843688994cffa36ae3df5420e2
이재영 <42104898+JaeYeong-Li@users.noreply.github.com>
2022-03-06 19:03:36 +0900

    feat #4 스킬 필터링 기능
    빨강(skill), 파랑(experience), 검정(cooperation) 버튼 클릭시 해당하는 스킬 보기를 생략하거나 다시 켤 수 있다.

--- frontend/src/components/skill/index.jsx
+++ frontend/src/components/skill/index.jsx
@@ -1,37 +1,47 @@
-/* eslint-disable array-callback-return */
+import React, {useState, useRef} from 'react';
 import './index.scss';
 
 const myData = {
     'language': [{key:'JS', state:'red'}, {key:'C/C++', state:'red'}, {key:'TS', state:'blue'}, {key:'JAVA(Android)', state:'blue'}],
-    'frontend': [{key:'React', state:'red'}, {key:'Webpack', state:'red'}, {key:'SCSS/Styled-component', state:'red'}],
+    'frontend': [{key:'React', state:'red'}, {key:'Webpack', state:'red'}, {key:'SCSS', state:'red'}, {key:'Styled-Component', state:'red'}],
     'backend': [{key:'Node.js', state:'red'}, {key:'mySQL', state:'blue'}, {key:'Sequelize', state:'blue'}],
     'cooperation': [{key:'Git', state:'black'}, {key:'Figma', state:'black'}]
 };
 
 const Skill = () => {
+    const [showSkills, setShowSKills] = useState({red:true, blue:true, black:true});
+    const mySkills = useRef(null);
+    const skillClickHandler = (e) => {
+        const pClassList = e.target.parentNode.classList;
+        if(!pClassList.contains('skillButton')) return;
+        const curColor = e.target.className;
+        const updatedSkills = Object.keys(showSkills)
+        .reduce((obj, key)=> {obj[key]= curColor===key ? !showSkills[key] : showSkills[key]; return obj}, {});
+        setShowSKills(updatedSkills);
+    }
     return (
-        <div className="skill">
-            <div className="skillButton">
-                <button className="black">●</button>
-                <button className="blue">●</button>
-                <button className="red">●</button>
+        <div className="skill sa sa-up">
+            <div className="skillButton hi">
+                <button data-tooltip-text = "cooperation" className="black" onClick={(e)=>{skillClickHandler(e)}}>●</button>
+                <button data-tooltip-text = "experience" className="blue" onClick={(e)=>{skillClickHandler(e)}}>●</button>
+                <button data-tooltip-text = "my skills"className="red" onClick={(e)=>{skillClickHandler(e)}}>●</button>
             </div>
-            <div className="mySkills">
+            <div className="mySkills" ref={mySkills}>
                 <div className="mySkill">
                     <p>language</p>
-                    {myData.language.map((e,i)=><p key={e.key} className={e.state}>{e.key}</p>)}
+                    {myData.language.map((e) => { return showSkills[e.state] ? <p key={e.key} className={e.state}>{e.key}</p> : null})}
                 </div>
                 <div className="mySkill">
                     <p>frontend</p>
-                    {myData.frontend.map((e,i)=><p key={e.key} className={e.state}>{e.key}</p>)}
+                    {myData.frontend.map((e) => { return showSkills[e.state] ? <p key={e.key} className={e.state}>{e.key}</p> : null})}
                 </div>
                 <div className="mySkill">
                     <p>backend</p>
-                    {myData.backend.map((e,i)=><p key={e.key} className={e.state}>{e.key}</p>)}
+                    {myData.backend.map((e) => { return showSkills[e.state] ? <p key={e.key} className={e.state}>{e.key}</p> : null})}
                 </div>
                 <div className="mySkill">
                     <p>cooperation</p>
-                    {myData.cooperation.map((e,i)=><p key={e.key} className={e.state}>{e.key}</p>)}
+                    {myData.cooperation.map((e) => { return showSkills[e.state] ? <p key={e.key} className={e.state}>{e.key}</p> : null})}
                 </div>
             </div>
         </div>
