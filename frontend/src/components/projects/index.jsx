44e19f8e059a976b69543871de73d9a439a455df
이재영 <42104898+JaeYeong-Li@users.noreply.github.com>
2022-03-06 19:38:25 +0900

    feat #7 프로젝트 좌우 버튼 이벤트

--- frontend/src/components/projects/index.jsx
+++ frontend/src/components/projects/index.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable jsx-a11y/alt-text */
 import './index.scss';
 import {useSelector} from 'react-redux';
+import React, {useState} from 'react';
 
 const Projects = ()=> {
     const myProjects = useSelector((state)=>state.projects.project);
-    let idx1=0, idx2=1;
+    const [cIdxs, setIdxs] = useState([0,1]);
+    const moveHandler = (e) => {
+        const cButton = e.target.parentNode.classList.contains('left') ? 'left': 'right';
+        let idx1, idx2;
+        if(cButton==='left') {
+            idx2 = cIdxs[1]-2 < 0 ? myProjects.length-1 : cIdxs[1]-2;
+            idx1 = idx2-1 < 0 ? myProjects.length-1 : idx2-1;
+        }
+        else {
+            idx1 = myProjects.length <= cIdxs[0]+2 ? 0 : cIdxs[0]+2;
+            idx2 = idx1===myProjects.length-1 ? 0 : idx1+1;
+        }
+        setIdxs([idx1,idx2]);
+    }
     const createCard = (idx)=> {
         return (
             <div className="project">
@@ -23,11 +37,11 @@ const Projects = ()=> {
         )
     }
     return (
-        <div className="projects">
-            <div className="moveButton left"><img src={process.env.PUBLIC_URL+'/icon/left.png'}/></div>
-            {createCard(idx1)}
-            {createCard(idx2)}
-            <div className="moveButton right"><img src={process.env.PUBLIC_URL+'/icon/right.png'}/></div>
+        <div className="projects sa sa-up">
+            <div className="moveButton left" onClick={(e)=>{moveHandler(e)}}><img src={process.env.PUBLIC_URL+'/icon/left.png'}/></div>
+            {createCard(cIdxs[0])}
+            {createCard(cIdxs[1])}
+            <div className="moveButton right"onClick={(e)=>{moveHandler(e)}}><img src={process.env.PUBLIC_URL+'/icon/right.png'}/></div>
         </div>
     )
 }
